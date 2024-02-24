0b61a8ff60e78fd6403eed316654f7200d751717
tigati <packudjj@gmail.com>
2024-02-24 00:03:37 -0800

    Basic Coding

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('Helldwqdwqdo')
+function Calculate_Function(){
+    const initialInvestment = parseFloat(document.getElementById('Initial_Investment').value)
+    const monthlyContribution = parseFloat(document.getElementById('Monthly_Contribution').value)
+    const lenghtOfTime = parseFloat(document.getElementById('Lenght_Of_Time').value)
+
+    return initialInvestment+monthlyContribution+lenghtOfTime;
+}
+
+
+
+
+
+
+document.getElementById('calculate').addEventListener('click', Calculate_Function() {
+    document.getElementById('total_amount').textContent = Calculate_Function();
+});
+
